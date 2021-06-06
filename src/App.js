9472448dfb37857d0c1d42a12b804a2f5c8d15d3
Temmy Alex <temmy.alex@gmail.com>
2021-06-06 12:57:22 +0700

    Get api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      myData: []
+    };
+  }
+
+  componentDidMount(){
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then((getData) => {
+        console.log(getData);
+        this.setState({
+          myData: getData.data
+        })
+      })
+  };
+
+  render(){
+    const data = this.state.myData.map((item, index) => {
+      const id = item.id;
+      const name = item.name;
+      const email = item.email;
+      const address = [
+        item.address.suite,
+        item.address.street,
+        item.address.city
+      ].join(', ');
+      return <tr>
+        <td key={id}>{id}</td>
+        <td>{name}</td>
+        <td>{email}</td>
+        <td>{address}</td>
+      </tr>
+    })
+
+    return (
+      <div>
+        <h1 className="text-center mb-5 mt-5">Get Data</h1>
+        <table className="table table-bordered table-striped">
+          <tbody>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Address</th>
+            </tr>
+            { data }
+          </tbody>
+        </table>
+      </div>
+    )
+  }
 }
 
 export default App;
